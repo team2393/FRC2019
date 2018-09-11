9e545cdce539195c9c40e04c6d1bede366f003d4
Kay Kasemir <kay.kasemir@gmail.com>
2018-09-11 10:50:12 -0400

    Use RobotController

--- src/robot/parts/USERButton.java
+++ src/robot/parts/USERButton.java
@@ -1,7 +1,7 @@
 package robot.parts;
 
 import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.hal.HALUtil;
+import edu.wpi.first.wpilibj.RobotController;
 
 /** Support for the 'USER' Button on the roboRIO.
  * 
@@ -21,9 +21,7 @@ public class USERButton extends Button
     /** @return <code>true</code> when the 'USER' button is pressed, otherwise <code>false</code> */
     public static boolean isPressed()
     {
-        // It's a little hard to guess that this is the way to read the 'USER' button,
-        // which is why this USERButton class was created
-        return HALUtil.getFPGAButton();
+        return RobotController.getUserButton();
     }
 
     @Override
