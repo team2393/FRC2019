73207443e68f00812783099b6c528bc93f124fef
Kay <kay.kasemir@gmail.com>
2018-09-13 16:57:41 -0400

    Robot demos package info

---
+++ src/robot/demos/package-info.java
@@ -0,0 +1,27 @@
+/**
+ * Demo Robots
+ * 
+ * <p>{@link robot.demos.LEDDemoRobot1}, {@link robot.demos.LEDDemoRobot2}:
+ * Blink LED
+ * 
+ * <p>{@link robot.demos.ServoDemoRobot}:
+ * Move a servo
+ * 
+ * <p>{@link robot.demos.LightSensorRobot}:
+ * Turn LED on when its dark
+ * 
+ * <p>{@link robot.demos.MeasurePeriodRobot}:
+ * How often is the 'periodic' code called?
+ * 
+ * <p>{@link robot.demos.DriveDemoRobot}:
+ * Drive when USER button is pressed
+ *
+ * <p>{@link robot.demos.GyroRobot}, {@link robot.demos.AccelerometerRobot}
+ * Read on-board sensors, with one way to visualize data in Accelerator demo
+ * 
+ * <p>{@link robot.demos.DashboardRobot}:
+ * Publish data to dashbaord
+ * 
+ */
+package robot.demos;
+
