b2d5e602d2b8611d6e34163c7897e1a42e4c456c
Kay <kay.kasemir@gmail.com>
2019-01-11 17:31:33 -0500

    Publish data rate on dashboard

--- src/main/java/robot/camera/CameraRobot.java
+++ src/main/java/robot/camera/CameraRobot.java
@@ -10,26 +10,36 @@ import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.cscore.VideoMode;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import robot.BasicRobot;
 
 /** Camera Tests */
 public class CameraRobot extends BasicRobot
 {
+	private CameraServer server;
+	private UsbCamera camera;
+
 	@Override
 	public void robotInit()
 	{
 		super.robotInit();
 	
 		// Publish image from USB camera
-		CameraServer server = CameraServer.getInstance();
-		UsbCamera camera = server.startAutomaticCapture();
+		server = CameraServer.getInstance();
+		camera = server.startAutomaticCapture();
 		CameraInfo.show(camera);
 		camera.setResolution(640, 480);
 		camera.setFPS(10);
 
-
 		Thread thread = new Thread(new Crosshair(server, camera));
-		thread.start();
-	
+		thread.start();	
 	}
+
+	@Override
+    public void robotPeriodic()
+    {
+		double rate = camera.getActualDataRate();
+		SmartDashboard.putNumber("Rate", rate);
+		SmartDashboard.putBoolean("Too Much", rate*8 > 4*1024*1024);
+    }
 }
