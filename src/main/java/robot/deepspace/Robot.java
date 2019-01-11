a17e04bbce028f1ee0e73e63785613cbab2eeb0d
FruityPig <fruitypigy@gmail.com>
2019-01-11 17:07:34 -0500

    Camera Measurements

--- src/main/java/robot/deepspace/Robot.java
+++ src/main/java/robot/deepspace/Robot.java
@@ -7,7 +7,9 @@
 
 package robot.deepspace;
 
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import robot.BasicRobot;
 
 /**
@@ -20,6 +22,13 @@ public class Robot extends BasicRobot
     {
         super.robotInit();
         // this is where the camera streaming begins
-        CameraServer.getInstance().startAutomaticCapture();
+        UsbCamera camera =  CameraServer.getInstance().startAutomaticCapture();
+        //160 by 120 at 10fps - 0.3mbs
+        //640 by 480 at 10fps - 3.1mbs
+        //640 by 480 at 15fps - 4.1mbs
+        //610 by 450 at 15fps - 3.4mbs
+        camera.setResolution(600, 440);
+        camera.setFPS(10);
+        
     }
 }
