9e678d122f746bfb9b514f41d690552a3b5197db
Kay <kay.kasemir@gmail.com>
2019-01-13 10:57:23 -0500

    Add PCM demo

---
+++ src/main/java/robot/demos/PneumaticDemo.java
@@ -0,0 +1,30 @@
+package robot.demos;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import robot.BasicRobot;
+
+/** Demo of turning output of Pneumatic Control Module on/off */
+public class PneumaticDemo extends BasicRobot
+{
+    private final Solenoid sol1 = new Solenoid(0);
+
+    @Override
+    public void robotInit()
+    {
+        super.robotInit();
+
+        // Allow control via dashboard
+        SmartDashboard.putData("Sol1", sol1);
+    }
+
+    @Override
+    public void autonomousPeriodic()
+    {
+        // Turn solenoid on/off every 2 seconds
+        if (((System.currentTimeMillis() / 2000) % 2) == 1)
+            sol1.set(true);
+        else
+            sol1.set(false);
+    }
+}
