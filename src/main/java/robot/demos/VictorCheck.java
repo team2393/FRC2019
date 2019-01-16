168a23044570740d07ede330902dc9c6e012c7d0
Kay <kay.kasemir@gmail.com>
2019-01-15 19:20:07 -0500

    Victor comments

--- src/main/java/robot/demos/VictorCheck.java
+++ src/main/java/robot/demos/VictorCheck.java
@@ -1,8 +1,6 @@
 package robot.demos;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -10,14 +8,12 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import robot.BasicRobot;
 import robot.parts.PDPController;
 
-/** Demo of turning output of Pneumatic Control Module on/off */
+/** Demo of basic speed control with VictorSPX */
 public class VictorCheck extends BasicRobot
 {
-    private final int PCM_ID = 0;
-
     // VictorSPX offers full access to the controller.
     // WPI_VictorSPX wraps that to add the basic set(speed)
-    // necessary for DifferentialDrive
+    // necessary for DifferentialDrive, in case we need that later.
     private final WPI_VictorSPX vic = new WPI_VictorSPX(1);
 
     private final Joystick joystick = new Joystick(0);
@@ -30,13 +26,17 @@ public class VictorCheck extends BasicRobot
         System.out.println(vic.getFirmwareVersion());
 
         // You can also configure the Victor via the Tuner application,
-        // but it's best to configuret them from the code at robot initialization
-        // because that allows swapping the actual Victor controller,
-        // needing only to set the ID, and the rest is then configured in here.
+        // but it's best to configure it from the code at robot initialization.
+        // That way, if we later need to replace the actual Victor controller,
+        // we only need to set the ID, and the rest will be configured on startup,
+        // no need to do that manually in the Tuner.
         vic.configFactoryDefault();
         vic.setNeutralMode(NeutralMode.Brake); // Brake or coast?
         vic.setInverted(false);                // Invert?
         vic.configOpenloopRamp(2.0);           // Use N seconds to ramp to full speed
+
+        // The WPI_VictorSPX can be placed on the dashboard to show some basic info
+        SmartDashboard.putData("Victor", vic);
     }
 
     @Override
@@ -44,8 +44,6 @@ public class VictorCheck extends BasicRobot
     {
         final double speed = - joystick.getRawAxis(PDPController.RIGHT_STICK_VERTICAL);
         vic.set(speed);
-
-        SmartDashboard.putNumber("Victor", vic.getSelectedSensorPosition());
     }
 
     @Override
