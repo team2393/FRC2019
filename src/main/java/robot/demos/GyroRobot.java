62485131cd2eda2687b77fc0b01c5332a98efc9e
Kay <kay.kasemir@gmail.com>
2019-01-26 19:05:58 -0500

    Gyro reset command

--- src/main/java/robot/demos/GyroRobot.java
+++ src/main/java/robot/demos/GyroRobot.java
@@ -1,6 +1,9 @@
 package robot.demos;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
+import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import robot.BasicRobot;
@@ -21,18 +24,23 @@ import robot.BasicRobot;
  */
 public class GyroRobot extends BasicRobot
 {
-    private Gyro gyro = new ADXRS450_Gyro();
+    private final Gyro gyro = new ADXRS450_Gyro();
+
+    private final Command reset = new InstantCommand(gyro::reset);
 
     @Override
     public void robotInit()
     {
         super.robotInit();
         System.out.println("Needs Gyro board in SPI");
+        reset.setRunWhenDisabled(true);
+        SmartDashboard.putData("Reset", reset);
     }
 
     @Override
 	public void robotPeriodic()
 	{
+        Scheduler.getInstance().run();
         double heading = gyro.getAngle();
         // Basics of 'printf' style formatting:
         //
