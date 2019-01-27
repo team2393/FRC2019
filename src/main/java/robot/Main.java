1e6183ea2160f44d9b8b2c704bc7ef6b4312784e
Kay <kay.kasemir@gmail.com>
2019-01-26 20:08:18 -0500

    Select robot

--- src/main/java/robot/Main.java
+++ src/main/java/robot/Main.java
@@ -2,7 +2,6 @@ package robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 import robot.deepspace.DeepspaceRobot;
-import robot.demos.GyroRobot;
 
 /** The java 'main' class */
 public final class Main
@@ -10,6 +9,6 @@ public final class Main
   public static void main(String... args)
   {
     // Change this to select which robot code to run
-    RobotBase.startRobot(GyroRobot::new);
+    RobotBase.startRobot(DeepspaceRobot::new);
   }
 }
