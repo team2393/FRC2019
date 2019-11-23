b5e260265045f5b990beed2940523829212e1550
unknown <team2393@laptop.org>
2019-11-22 19:01:25 -0500

    Prepare PiCameraRobot

---
+++ src/main/java/robot/picamera/PiCameraRobot.java
@@ -0,0 +1,23 @@
+package robot.picamera;
+
+import robot.BasicRobot;
+
+/** Robot with Camera on Pi
+ * 
+ *  Pi runs PiVionDemo example which detects 
+ *  colored blob and updates 'Direction'
+ * 
+ *  0: blob is in center of picture, or we don't know
+ *  Positive: Blob is N pixel to the right of center
+ *  Negative: ... to the left of center
+ */
+public class PiCameraRobot extends BasicRobot
+{
+  // TODO Add Joystick
+
+  // TODO Add Servo
+
+  // TODO in teleop, control servo with joystick
+
+  // TODO in autonomouse, control servo based on 'Direction'
+}
