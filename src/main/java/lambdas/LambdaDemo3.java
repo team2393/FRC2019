8de220bb9fdf1f0c8f4235203673c793051dce0b
unknown <team2393@laptop.org>
2019-11-27 08:17:11 -0500

    Closer to new WPIlib

--- src/main/java/lambdas/LambdaDemo3.java
+++ src/main/java/lambdas/LambdaDemo3.java
@@ -1,5 +1,6 @@
 package lambdas;
 
+import java.util.function.DoubleConsumer;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.Encoder;
@@ -11,22 +12,28 @@ public class LambdaDemo3
     // A PID controller needs the current value
     // of what it wants to control, like the "position"
     // or "height".
+    // The result then needs to be applied to for
+    // example a motor controller.
     //
-    // The new WPIlib contains an updated PIDController class
+    // The new WPIlib contains an updated PIDController & PIDCommand class
     // that uses lambdas, i.e. a @FunctionalInterface for that.
     //
-    // Basics of new PID controller (doing only P control)
-    static class PIDController
+    // Basics of new PID (doing only P control)
+    static class PIDCommand
     {
         private double setpoint = 0;
         private double P;
-        // DoubleSupplier is a standard Java @FunctionalInterface
+        // DoubleSupplier & Consumer are a standard Java @FunctionalInterfaces
         private DoubleSupplier current_value_supplier;
+        private DoubleConsumer correction_user;
 
-        public PIDController(double P, DoubleSupplier current_value_supplier)
+        public PIDCommand(double P,
+                          DoubleSupplier current_value_supplier,
+                          DoubleConsumer correction_user)
         {
             this.P = P;
             this.current_value_supplier = current_value_supplier;
+            this.correction_user = correction_user;
         }
 
         public void setSetpoint(double new_setpoint)
@@ -34,21 +41,27 @@ public class LambdaDemo3
             setpoint = new_setpoint;
         }
 
-        double calculate()
+        public void execute()
         {
+            // Get the current value
             double current_value = current_value_supplier.getAsDouble();
+            // P(ID) algorithm
             double error = setpoint - current_value;
-            return P*error;
+            double correction = P*error;
+            // Apply the correction
+            correction_user.accept(correction);
         }
     }
 
+    // -------------------------------------------------------------
 
-
-    // Basic idea for connecting that PIDController to a lift:
+    // Basic idea for connecting that PIDCommand to a lift:
     private Victor lift_motor = new Victor(1);
     private Encoder lift_position = new Encoder(2, 3);
-    private PIDController lift_pid = new PIDController(0.5, () -> lift_position.getDistance());
-    // Or: ...                       new PIDController(0.5, lift_position::getDistance);
+    private PIDCommand lift_pid = new PIDCommand(0.5,
+                                                 () -> lift_position.getDistance(),
+                                                 result -> lift_motor.setSpeed(result));
+    // Or: ... new PIDCommand(0.5, lift_position::getDistance, lift_motor::setSpeed);
 
     void autonomousInit()
     {
@@ -57,8 +70,9 @@ public class LambdaDemo3
 
     void autonomousPeriodic()
     {
-        // PID has setpoint, P, and a lambda for getting the current position.
-        // We can simply ask it to calculate the motor speed:
-        lift_motor.setSpeed(lift_pid.calculate());
+        // PID has setpoint, P, and lambdas for getting the current position
+        // and setting the motor.
+        // We can simply ask it to perform the control:
+        lift_pid.execute();
     }
 }
